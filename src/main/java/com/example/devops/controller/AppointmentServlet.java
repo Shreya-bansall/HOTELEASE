6799c960bce0f5748f338345c037f86c2d5670d9
Shreya-bansall <shreyab2400@gmail.com>
2023-05-02 12:30:47 +0530

    update

--- src/main/java/com/example/devops/controller/AppointmentServlet.java
+++ src/main/java/com/example/devops/controller/AppointmentServlet.java
@@ -1,67 +1,68 @@
 package com.example.devops.controller;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
-@WebServlet("/bookAppointment")
 public class AppointmentServlet extends HttpServlet {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/devops";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "123456";
+    private Connection conn;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String checkin = request.getParameter("checkin");
-        String checkout = request.getParameter("checkout");
-        int adults = Integer.parseInt(request.getParameter("adults"));
-        int children = Integer.parseInt(request.getParameter("children"));
-        String roomType = request.getParameter("roomType");
+    // Initialize the database connection
+    public void init() throws ServletException {
+        String url = "jdbc:mysql://localhost:3306/devops"; // Replace with your database URL
+        String user = "root"; // Replace with your database user
+        String password = "123456"; // Replace with your database password
 
-        // Insert data into the database
-        synchronized (this) {
-            try {
-                Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                String sql = "INSERT INTO appointments (checkin, checkout, adults, children, roomType) VALUES (?, ?, ?, ?, ?)";
-                PreparedStatement statement = conn.prepareStatement(sql);
-                statement.setString(1, checkin);
-                statement.setString(2, checkout);
-                statement.setInt(3, adults);
-                statement.setInt(4, children);
-                statement.setString(5, roomType);
-                statement.executeUpdate();
-                statement.close();
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new ServletException("Database access error", e);
-            }
-        }
-
-        // Perform web scraping using Selenium
         try {
-            System.setProperty("webdriver.chrome.driver", "C:/chromedriver");
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://www.google.com");
-            driver.findElement(By.linkText("About")).click();
-            driver.quit();
-        } catch (Exception e) {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new ServletException("JDBC driver not found.", e);
+        } catch (SQLException e) {
+            throw new ServletException("Unable to connect to the database.", e);
+        }
+    }
+
+    // Process the form submission
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // Retrieve the form data
+            String checkin = request.getParameter("checkin");
+            String checkout = request.getParameter("checkout");
+            int adults = Integer.parseInt(request.getParameter("adults"));
+            int children = Integer.parseInt(request.getParameter("children"));
+            String roomType = request.getParameter("roomType");
+
+            // Create the SQL INSERT statement
+            String sql = "INSERT INTO bookings (checkin, checkout, adults, children, room_type) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, checkin);
+            statement.setString(2, checkout);
+            statement.setInt(3, adults);
+            statement.setInt(4, children);
+            statement.setString(5, roomType);
+
+            // Execute the SQL INSERT statement
+            statement.executeUpdate();
+
+            // Redirect the user to a confirmation page
+            response.sendRedirect("confirmation.jsp");
+        } catch (SQLException e) {
+            throw new ServletException("Unable to store the form data in the database.", e);
+        }
+    }
+
+    // Close the database connection
+    public void destroy() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
-            throw new ServletException("Web scraping error", e);
-        } finally {
-            // Redirect to confirmation.jsp
-            response.sendRedirect("src/main/webapp/confirmation.jsp");
         }
     }
 }
